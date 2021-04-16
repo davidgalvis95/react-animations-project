4a342ee4c4d54a7012741171232973772e9d0398
David Galvis <ivan.galvis@project44.com>
2021-04-15 21:39:15 -0500

    using ReactTransitionGroup

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from "react";
+import Transition from 'react-transition-group';
 
 import "./App.css";
 import Modal from "./components/Modal/Modal";
@@ -9,7 +10,8 @@ class App extends Component {
 
     state = {
         modalIsOpen: null,
-        modalIsInitiated: null
+        modalIsInitiated: null,
+        showBlock: false
     }
 
     showModal = () => {
@@ -24,6 +26,25 @@ class App extends Component {
         return (
             <div className="App">
                 <h1>React Animations</h1>
+                <button className="Button" onClick={() => this.setState(prevState =>({showBlock: !prevState.showBlock}))}>Toggle</button>
+                <br/>
+                {/*The mount on enter and unmount on exit are properties that make the object take off from the DOM*/}
+                <Transition
+                    in={this.state.showBlock}
+                    timeout={1000}
+                    mountOnEnter={}
+                    unmountOnExit={}>
+                    {state => (
+                        <div style={{
+                            backgroundColor: "red",
+                            width: 100,
+                            height: 100,
+                            margin: "auto",
+                            transition: 'opacity 1s ease-out',
+                            opacity: state === 'exiting' ? 0 : 1
+                        }}/>
+                    )}
+                </Transition>
                 {/*This solution is not complete at all because the null happens before the closed gets executed (it is rendered before)*/}
                 {this.state.modalIsOpen? <Modal show={this.state.modalIsOpen} closed={this.closeModal}/>: null}
                 {this.state.modalIsOpen?<Backdrop show={this.state.modalIsOpen}/>: null}
